1451d1f0d786aa86724e3c04ad24dbe07f860d16
Lucas Furtado <lucas_shiguioka@hotmail.com>
2024-03-25 16:31:21 -0300

    tema claro correções

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="w-0 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 dark:bg-slate-700 bg-slate-50 dark:text-green-500 text-green-600 h-[calc(100vh-80px)] left-0 top-[80px] fixed">
+    class="w-0 sm:w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 dark:bg-slate-700 bg-slate-200 dark:text-green-500 text-green-600 h-[calc(100vh-80px)] left-0 top-[80px] fixed">
     <div class="w-full m-1 px-4 py-3 font-bold hover:bg-slate-700 bg-slate-800 rounded-l-full ">
         Home
     </div>
