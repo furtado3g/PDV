8151a928d214988a78371b74cbfdcd9a2375d8d5
Lucas Furtado <lucas_shiguioka@hotmail.com>
2024-03-25 16:14:36 -0300

    sidebar has been finished

---
+++ resources/views/DashBoard.blade.php
@@ -0,0 +1,7 @@
+<x-page :title="'Home'">
+    <x-navbar/>
+    <x-side-bar/>
+    <x-content>
+
+    </x-content>
+</x-page>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<div class="w-full top-o fixed">
+    <div class="w-full flex items-center p-4 dark:bg-slate-900 bg-slate-50 h-20">
+        <h4 class="text-green-500 font-extrabold text-2xl md:mr-10">
+            Furtado3g
+        </h4>
+        <div class="w-[300px]">
+            <input type="text" name="" id="" placeholder="Pesquisar" class="w-full p-3 rounded-full">
+        </div>
+        <a href="" class="text-green-500 hover:text-green-300 p-2 rounded-md font-bold w-40 ml-auto text-center">Lucas Furtado</a>
+    </div>
+</div>
