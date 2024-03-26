9fe79838b83de4198008cb7339f5a29f94e9262c
Lucas Furtado <lucas_shiguioka@hotmail.com>
2024-03-26 16:45:11 -0300

    adicionado o react com inertia ao projeto

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,7 @@
+
+export default function ApplicationLogo(props) {
+
+    return (
+        <img src="/storage/logo.png" alt="logo" {...props} />
+    );
+}
---
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -0,0 +1,50 @@
+import InputError from '@/Components/InputError';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import GuestLayout from '@/Layouts/GuestLayout';
+import { Head, useForm } from '@inertiajs/react';
+
+export default function ForgotPassword({ status }) {
+    const { data, setData, post, processing, errors } = useForm({
+        email: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('password.email'));
+    };
+
+    return (
+        <GuestLayout>
+            <Head title="Forgot Password" />
+
+            <div className="mb-4 text-sm text-gray-600">
+                Esqueceu sua senha? Sem problemas. Basta nos informar seu endereço de e-mail e enviaremos uma senha por e-mail
+                link de redefinição que permitirá que você escolha um novo.
+            </div>
+
+            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+
+            <form onSubmit={submit}>
+                <TextInput
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    className="mt-1 block w-full"
+                    isFocused={true}
+                    onChange={(e) => setData('email', e.target.value)}
+                />
+
+                <InputError message={errors.email} className="mt-2" />
+
+                <div className="flex items-center justify-end mt-4">
+                    <PrimaryButton className="ms-4" disabled={processing}>
+                        Enviar link para redefinição
+                    </PrimaryButton>
+                </div>
+            </form>
+        </GuestLayout>
+    );
+}
